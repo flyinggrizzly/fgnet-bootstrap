08fb69a30d955c17732304342b95840702a6d691
Sean DMR <sean@flyinggrizzly.net>
2019-04-24 21:24:33 +0100

    Move Layout to sensible location

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,5 @@
 import React from "react"
-import Layout from "../components/layout/Layout"
-
-import Hero from "../components/hero/Hero"
-import Summary from "../components/summary/Summary"
-import Projects from "../components/projects/Projects"
-
-//import bootstrap styles
-import "../bootstrap/css/bootstrap.min.css"
+import Layout from "components/layout"
 
 //import my custom styles
 import "./index.css"
