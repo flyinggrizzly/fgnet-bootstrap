eed002bdcf9737027ec5470511115499527f7fa8
Sean DMR <sean@flyinggrizzly.net>
2019-05-01 15:13:36 +0100

    Add sidebar title and image

--- src/components/layout/sidebar.js
+++ src/components/layout/sidebar.js
@@ -5,6 +5,8 @@ import sidebarStyles from 'styles/sidebar.module.css'
 const Sidebar = () => (
   <>
     <section className={ sidebarStyles.sidebar }>
+      <img src="/flying-grizzly.png" alt="a flying grizzly bear" />
+      <h1>Flying Grizzly</h1>
     </section>
   </>
 )
