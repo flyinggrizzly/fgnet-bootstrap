41c816ab274acb3caf35ebda3b8b2e22153edf7f
Sean DMR <sean@flyinggrizzly.net>
2019-05-10 23:34:41 +0100

    Sketch out basics of homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,70 @@
-import React from "react"
-import Layout from "components/layout"
+import React from 'react'
+import { Link, graphql } from 'gatsby'
+import Layout from 'components/layout'
+import CTA from 'components/cta'
 
-//import my custom styles
-import "./index.css"
+import styles from 'styles/blog_index.module.css'
 
-export default () => (
-  <div className="App">
-    <Layout>
-    </Layout>
-  </div>
-)
+const BlogIndex = ({ data }) => {
+  const siteTitle = data.site.siteMetadata.title
+  const posts = data.allMdx.edges
+
+  return (
+    <div className="App">
+      <Layout>
+        { posts.map(post => {
+          let { excerpt, fields: { date, title, slug } } = post.node
+
+          return (
+            <PostPreview
+              title={ title }
+              date={ date }
+              slug={ slug }
+              excerpt={ excerpt }
+            />
+          )
+        }) }
+      </Layout>
+    </div>
+  )
+}
+export default BlogIndex
+
+const PostPreview = ({ title, date, slug, excerpt }) => {
+  return (
+    <section className={ styles.postPreview }>
+      <h1 className={ styles.postPreviewTitle }>
+        <Link to={ slug }>{ title }</Link>
+      </h1>
+      <h3 className={ styles.postPreviewDate }>{ date }</h3>
+      <div className={ styles.postPreviewExerpt }>
+        { excerpt }
+      </div>
+      <CTA target={ slug }>Read post</CTA>
+    </section>
+  )
+}
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    allMdx(
+      sort: { fields: [ fields___date ], order: DESC }
+    ) {
+      edges {
+        node {
+         excerpt
+          fields {
+            slug
+            date
+            title
+          }
+        }
+      }
+    }
+  }
+`
